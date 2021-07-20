6bd455de678bc684bd08f9a3be8ded2ad65ea901
1953902 高杨帆 <1030587461@qq.com>
2021-07-20 18:02:02 +0800

    进度提交，方案有变，先搜论文看

---
+++ Try_on/js/tryon.js
@@ -0,0 +1,98 @@
+// using Engine.js
+var engine = new Engine();
+// GLBLoader
+var loaderGLB = new THREE.GLTFLoader();
+// array of bone inverses
+var boneArray = [];
+// array of skinned meshes
+var skinnedMesh = []
+// ???
+var obj = new THREE.Object3D();
+
+// initialize skinnedMesh array
+function initSkinnedMesh(glb)
+{
+    glb.scene.traverse(node=> {
+        if (node instanceof THREE.SkinnedMesh) {
+            console.log(node);
+            skinnedMesh.push(node);
+        }
+    })
+}
+
+// initialize bone inverses
+function initBoneInverse(boneArray,skinnedMesh)
+{
+    var boneInverse = skinnedMesh[0].skeleton.boneInverses;
+    for(var i = 0; i<boneInverse.length;i++)
+    {
+        var copy = new THREE.Matrix4().copy(boneInverse[i]);
+        boneArray.push(copy);
+    }
+}
+
+// change the scale of bones
+function changeScale(skinnedMesh,adjustIndex,scale)
+{
+    // console.log(skinnedMesh);
+    var bone = new THREE.Matrix4().copy(boneArray[adjustIndex]);
+    // var scale = new THREE.Vector3(2,1,1);
+    // console.log(bone.elements)
+    bone.scale(scale);
+    // console.log(bone.elements)
+    for(var i = 0;i<skinnedMesh.length;i++)
+    {
+        skinnedMesh[i].skeleton.boneInverses[adjustIndex] = bone;
+    }
+}
+
+// change the weight of a bone
+// proved not effective
+/*function changeWeight(meshIndex)
+{
+    var weight = skinnedMesh[meshIndex].geometry.attributes.skinWeight;
+    // console.log(weight);
+    var weightArray = weight.array;
+
+    var itemSize = 4;
+    for(var i = 0;i<weightArray.length;++i)
+    {
+        if(i%itemSize === 0)
+        {
+            weightArray[i] = 1;
+        }
+        else
+        {
+            weightArray[i] = 0;
+        }
+    }
+    console.log(weightArray);
+}*/
+
+// load a model to a webpage
+function loadGLB()
+{
+    loaderGLB.load("model/people/try.glb",(glb)=>
+    {
+        var scene = glb.scene;
+        initSkinnedMesh(glb);
+        // for(var i = 0;i<skinnedMesh.length;i++)
+        // {
+        //     changeWeight(i);
+        // }
+        initBoneInverse(boneArray,skinnedMesh);
+        // console.log(skinnedMesh);
+        obj.add(scene);
+        // console.log(obj);
+        obj.children[0].scale.set(100,100,100);
+        // change(skinnedMesh,0,new THREE.Vector3(2,1,1))
+        // change(skinnedMesh,4,new THREE.Vector3(2,2,1));
+        changeScale(skinnedMesh,17,new THREE.Vector3(1.5,1,1));
+        engine.scene.add(obj);
+
+    })
+
+}
+
+engine.start();
+loadGLB();
